b152bf75c054176c54f92e6c645bb755cbd4bc6a
wangjianyu-1 <513596660@qq.com>
2021-08-01 15:34:33 +0800

    完成数据字典初始化到application

--- CRM/src/main/java/com/bjpowernode/crm/settings/service/impl/DicServiceImpl.java
+++ CRM/src/main/java/com/bjpowernode/crm/settings/service/impl/DicServiceImpl.java
@@ -2,12 +2,31 @@ package com.bjpowernode.crm.settings.service.impl;
 
 import com.bjpowernode.crm.settings.dao.DicTypeDao;
 import com.bjpowernode.crm.settings.dao.DicValueDao;
+import com.bjpowernode.crm.settings.domain.DicType;
+import com.bjpowernode.crm.settings.domain.DicValue;
 import com.bjpowernode.crm.settings.service.DicService;
 import com.bjpowernode.crm.utils.SqlSessionUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DicServiceImpl implements DicService {
 
     private DicTypeDao dicTypeDao = SqlSessionUtil.getSqlSession().getMapper(DicTypeDao.class);
     private DicValueDao dicValueDao  = SqlSessionUtil.getSqlSession().getMapper(DicValueDao.class);
 
+    @Override
+    public Map<String, List<DicValue>> getAllDic() {
+
+        Map<String, List<DicValue>> map = new HashMap<>();
+
+        List<DicType> dicTypes = dicTypeDao.getAllDicType();
+
+        for(DicType dt:dicTypes){
+            map.put(dt.getCode(),dicValueDao.getAllDicValue(dt.getCode()));
+        }
+
+        return map;
+    }
 }
